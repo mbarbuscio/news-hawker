9bb0290b7eb305d0f9576e7b2f238abadd41d665
Mariano Barbuscio <marianobarbuscio@MacBook-Pro-de-Mariano.local>
2018-06-16 16:32:45 -0300

    Added Prod Config Added local countries flags

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDHfcxY2J5APAsU4ajr5i0Q-HlDBS-xEUU",
+    authDomain: "newsfeed-db.firebaseapp.com",
+    databaseURL: "https://newsfeed-db.firebaseio.com",
+    projectId: "newsfeed-db",
+    storageBucket: "newsfeed-db.appspot.com",
+    messagingSenderId: "418826619729"
+  },
+  newsApi: {
+    apiKey: "ae7efcbff7794e01847d3d0c9b4344de",
+    baseUrl: "https://newsapi.org"
+  }
 };
